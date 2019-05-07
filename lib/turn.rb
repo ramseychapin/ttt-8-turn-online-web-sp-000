63535d5d8390407499bed1c0846805ea19b85e5d
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-07 01:53:36 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,6 +7,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(user_number)
+  index = user_number.to_i
+  index -= 1
+  return index
 
 # code your #valid_move? method here
 def valid_move?(board, position)
