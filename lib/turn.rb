5353fc87e5a9b12511926df7dfc0e5b8077e5743
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-07 01:47:22 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,17 @@
+# code your #valid_move? method here
+def valid_move?(board, position)
+  if position.between?(0,8) && !position_taken?(board, position)
+    return true
+  else
+    return false
+  end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
