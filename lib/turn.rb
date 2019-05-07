0e81cd280f478e1a2e63db1f417017c835cab3cc
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-07 01:57:20 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,50 @@
+#displays a tic tac toe board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_number)
+  index = user_number.to_i - 1
+  return index
+end
+
+# code your #valid_move? method here
+def valid_move?(board, position)
+  if position.between?(0,8) && !position_taken?(board, position)
+    return true
+  else
+    return false
+  end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
+
+#takes a user's position number, board, and places X or O character in that position
+def move(board, index, character = "X")
+  board[index] = character
+  return board
+end
+
+#CLI. Asks for a number, checks if valid,
+def turn(board)
+  puts "Please enter 1-9:"
+  num = gets.chomp
+  index = input_to_index(num)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
