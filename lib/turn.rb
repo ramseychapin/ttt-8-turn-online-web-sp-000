89a0873b987cde0755053bbb925dc9213e110644
Ramsey Chapin <ramsey@ramseychapin.com>
2019-05-07 01:49:56 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,13 @@
+#displays a tic tac toe board
+def diplay_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
 # code your #valid_move? method here
 def valid_move?(board, position)
   if position.between?(0,8) && !position_taken?(board, position)
